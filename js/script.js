62bfeaa5575184cace7fe1b409fe8320afb85357
daprel <daffarial18@gmail.com>
2025-11-21 21:32:06 +0700

    final

--- js/script.js
+++ js/script.js
@@ -10,6 +10,18 @@ function welcomeMessage() {
     console.log("Welcome," + userName + "!");
 }
 
-function validateForm() {
+function validateForm(e) {
+    e.preventDefault(); // cegah reload
 
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+    if (name === '' || email === '' || message === '') {
+    alert('Nama, email, dan pesan wajib diisi.');
+    return;
+  }
+
+    document.getElementById("output").innerText = "Halo! " + name + " dengan email " + email + "\n Pesan Anda: " + message + " (pesan kamu baru nyampe sini karena webnya belum jadi <3)";
+    console.log("Halo!" + name + "dengan email " + email + " Pesan Anda: " + message);
 }
